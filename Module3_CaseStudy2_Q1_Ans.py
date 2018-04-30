a26f4239f848595e6ccd2c26ad7736d03b707535
Diwakar Kumar <diwakarkumar.ind@gmail.com>
2018-04-30 21:21:00 +0530

    Domain Banking Marketing

--- Module3_CaseStudy2_Q1_Ans.py
+++ Module3_CaseStudy2_Q1_Ans.py
@@ -1,30 +1,3 @@
-# import csv
-# bankData = open("C:\\Temp\\bank-data.csv", "r")
-# # Set up CSV reader and process the header
-# csvReader = csv.reader(bankData)
-# header = csvReader.next()
-# ageIndex = header.index("age")
-# jobIndex = header.index("job")
-# maritalIndex = header.index("marital")
-# yIndex = header.index("y")
-#
-# # Make an empty list
-# customerList = []
-# jobList=['management','admin','entrepreneur','services','self-employed']
-# # Loop through the lines in the file and get each coordinate
-# for row in csvReader:
-#     age = row[ageIndex]
-#     job = row[jobIndex]
-#     # marital = row[maritalIndex]
-#     # y  = row[yIndex]
-#     customerList.append([age, job])
-#
-# print customerList
-# # strjob=raw_input('Enter profession to check:')
-# # for itm in customerList:
-# #     if strjob == itm:
-# #         print itm
-
 campaign_data = open('C:\\Temp\\bank-data.csv','r')
 i = 0;
 
